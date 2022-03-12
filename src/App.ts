cd5f1950a6b9c30940e0e366da182a51fd6eaa6a
Mickey <michael.cook@techdept.co.uk>
2022-03-12 16:32:30 +0000

    Properly assigning created p5 engine to export - mainly for intellisense

--- src/App.ts
+++ src/App.ts
@@ -6,9 +6,6 @@ export class App {
     let c = p5.color(r, 0, 0);
     p5.fill(c);
     p5.noStroke();
-    p5.rect(0, 0, p5.width, p5.height);
-    if (Math.random() > 0.9) {
-      console.log(dt);
-    }
+    p5.ellipse(0, 0, p5.width, p5.height);
   }
 }
